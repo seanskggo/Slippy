a111cde9d876cc94fb236f3bf80b0427f76e7815
z5310199 <z5310199@cse.unsw.edu.au>
2022-07-25 18:20:18 +1000

    progress

--- sed_parser.py
+++ sed_parser.py
@@ -24,12 +24,12 @@ class SedParser():
 
 def format_command(sed):
 
-    def format_sed(sed, cmd):
-        pre, cmd, post = get_pre_and_postfix(sed, cmd)
+    def format_sed(sed, cmd, delimiter='/'):
+        pre, cmd, post = get_pre_and_postfix(sed, cmd, delimiter)
         return {
-            "prefix": categorise_affix(pre),
+            "prefix": categorise_affix(pre, '/'),
             "command": cmd,
-            "postfix": categorise_affix(post)
+            "postfix": categorise_affix(post, delimiter)
         }
 
     if sed == '': 
@@ -40,8 +40,11 @@ def format_command(sed):
         return format_sed(sed, 'q')
     elif re.search('^(|[0-9]+|\/.+\/)d$', sed):
         return format_sed(sed, 'd')
-    elif re.search('^(|[0-9]+|\/.+\/)s\/.+\/.*\/g?$', sed):
-        return format_sed(sed, 's')
+    elif re.search('^(|[0-9]+|\/.+\/)s.*g?$', sed):
+        d = re.search('s(\S).*g?$', sed).group(1)
+        if not re.search(f'^(|[0-9]+|\/.+\/)s{d}.+{d}.*{d}g?$', sed):
+            throw_error()
+        return format_sed(sed, 's', d)
     else:
         throw_error()
     
@@ -65,9 +68,9 @@ def is_convertible_to_int(num):
 # Given a command regex of form <prefix>command<postfix>, 
 # return a tuple of three elements of (prefix, command, postfix)
 # e.g. 2s/a// => (2, s, '/a//')
-def get_pre_and_postfix(sed, cmd):
+def get_pre_and_postfix(sed, cmd, d):
     prefix = re.search(f'^(|[0-9]+|\/.+\/){cmd}', sed)
-    postfix = re.search(f'{cmd}(\/.+\/.*\/g?)$', sed)
+    postfix = re.search(f'{cmd}({d}.+{d}.*{d}g?)$', sed)
     return (
         prefix.group(1) if prefix else '', 
         cmd, 
@@ -76,9 +79,10 @@ def get_pre_and_postfix(sed, cmd):
 
 # Given an affix, categorise it into either a number or a regex
 # and return the filtered result in an object
-def categorise_affix(affix):
+def categorise_affix(affix, d):
     if not is_convertible_to_int(affix):
-        return { "affix": re.sub('\/(.*)\/', '\g<1>', affix), "is_regex": True }
+        aff = re.sub(f'{d}(.*){d}', '\g<1>', affix).split(d)
+        return { "affix": aff[0] if len(aff) <= 1 else aff, "is_regex": True }
     elif int(affix) > 0:
         return { "affix": int(affix), "is_regex": False }
     else:
