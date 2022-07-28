f694bc0b5e4c1d191cf4b15e29ca5021b052faa6
z5310199 <z5310199@cse.unsw.edu.au>
2022-07-28 20:00:44 +1000

    progress

--- sed_parser.py
+++ sed_parser.py
@@ -7,6 +7,9 @@
 import sys
 import re
 
+# Possible prefix regex
+PREFIX = '(\$|[0-9]+|\/.+\/)'
+
 class SedParser():
     def __init__(self, sed):
 
@@ -34,15 +37,15 @@ def format_command(sed):
 
     if sed == '': 
         return format_sed(sed, '')
-    elif re.search('^(|\$|[0-9]+|\/.+\/)p$', sed):
+    elif re.search(f'^({PREFIX},)?({PREFIX})?p$', sed):
         return format_sed(sed, 'p')
-    elif re.search('^(|\$|[0-9]+|\/.+\/)q$', sed):
+    elif re.search(f'^({PREFIX},)?({PREFIX})?q$', sed):
         return format_sed(sed, 'q')
-    elif re.search('^(|\$|[0-9]+|\/.+\/)d$', sed):
+    elif re.search(f'^({PREFIX},)?({PREFIX})?d$', sed):
         return format_sed(sed, 'd')
-    elif re.search('^(|\$|[0-9]+|\/.+\/)s.*g?$', sed):
+    elif re.search(f'^({PREFIX},)?({PREFIX})?s.*g?$', sed):
         d = re.search('s(\S).*g?$', sed).group(1)
-        if not re.search(f'^(|\$|[0-9]+|\/.+\/)s{d}.+{d}.*{d}g?$', sed):
+        if not re.search(f'^({PREFIX},)?({PREFIX})?s{d}.+{d}.*{d}g?$', sed):
             throw_error()
         return format_sed(sed, 's', d)
     else:
@@ -69,7 +72,7 @@ def is_convertible_to_int(num):
 # return a tuple of three elements of (prefix, command, postfix)
 # e.g. 2s/a// => (2, s, '/a//')
 def get_pre_and_postfix(sed, cmd, d):
-    prefix = re.search(f'^(|\$|[0-9]+|\/.+\/){cmd}', sed)
+    prefix = re.search(f'^(({PREFIX},)?({PREFIX})?){cmd}', sed)
     postfix = re.search(f'{cmd}({d}.+{d}.*{d}g?)$', sed)
     return (
         prefix.group(1) if prefix else '', 
