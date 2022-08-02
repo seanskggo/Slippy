b50f11b3e6ba9130a4dc5f68f119bb1700866436
z5310199 <z5310199@cse.unsw.edu.au>
2022-08-02 18:02:38 +1000

    add test files

---
+++ tests/test06.sh
@@ -0,0 +1,60 @@
+#!/bin/dash
+
+######################################################################
+# Test Script No. 6
+# Subset 2
+# 
+# Testing commands:
+# => q - quit command
+#
+# ASSUMES ALL RELEVANT FILES FRO SLIPPY ARE IN PATH
+######################################################################
+
+######################################################################
+# User Slippy Response File
+######################################################################
+
+test_commands () {
+
+    mkdir temp && cp slippy args_parser.py sed_parser.py temp && cd temp || exit
+
+    echo "test" | ./slippy 'q'
+
+    cd .. && rm -rf temp
+
+}
+
+######################################################################
+# Expected Slippy Response File
+######################################################################
+
+make_answers () {
+
+    mkdir temp && cd temp || exit
+
+    echo "test" | 2041 slippy 'q'
+
+    cd .. && rm -rf temp
+
+}
+
+######################################################################
+# Outcome
+######################################################################
+
+test_commands > a 2>&1
+make_answers > b 2>&1
+
+if [ -z "$(diff a b)" ]
+then 
+    echo "PASSED"
+else
+    echo "FAILED"
+    echo "--------------- ANSWER ---------------"
+    cat a
+    echo "-------------- EXPECTED --------------"
+    cat b
+    echo "--------------------------------------"
+fi
+
+rm a b
