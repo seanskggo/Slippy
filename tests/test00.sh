b7dcb7a422292cf31e0e03e969ef6f4cd39e56b8
z5310199 <z5310199@cse.unsw.edu.au>
2022-08-02 18:29:21 +1000

    test0 done

--- tests/test00.sh
+++ tests/test00.sh
@@ -18,7 +18,21 @@ test_commands () {
 
     mkdir temp && cp slippy args_parser.py sed_parser.py temp && cd temp || exit
 
-    echo "test" | ./slippy 'q'
+    seq 1 10 | ./slippy 'hq'       # error check 
+    seq 1 10 | ./slippy '*q'       # error check 
+    seq 1 10 | ./slippy '^q'       # error check 
+
+    echo "test" | ./slippy 'q'      # normal case
+    echo "" | ./slippy 'q'          # empty case
+    seq 1 10 | ./slippy 'q'         # sequence 
+    seq 1 10 | ./slippy '4q'        # sequence end at 4th line
+    seq 1 20 | ./slippy '/2./q'     # sequence with regex quit at 20
+
+    echo "test" | ./slippy 'p'      # normal case
+    echo "" | ./slippy 'p'          # empty case
+    seq 1 10 | ./slippy 'p'         # sequence 
+    seq 1 10 | ./slippy '4p'        # sequence print additional 4th line
+    seq 1 20 | ./slippy '/2./p'     # sequence print additional for 20s
 
     cd .. && rm -rf temp
 
@@ -32,7 +46,21 @@ make_answers () {
 
     mkdir temp && cd temp || exit
 
-    echo "test" | 2041 slippy 'q'
+    seq 1 10 | 2041 slippy 'hq'       # error check 
+    seq 1 10 | 2041 slippy '*q'       # error check 
+    seq 1 10 | 2041 slippy '^q'       # error check 
+
+    echo "test" | 2041 slippy 'q'      # normal case
+    echo "" | 2041 slippy 'q'          # empty case
+    seq 1 10 | 2041 slippy 'q'         # sequence 
+    seq 1 10 | 2041 slippy '4q'        # sequence end at 4th line
+    seq 1 20 | 2041 slippy '/2./q'     # sequence with regex quit at 20
+
+    echo "test" | 2041 slippy 'p'      # normal case
+    echo "" | 2041 slippy 'p'          # empty case
+    seq 1 10 | 2041 slippy 'p'         # sequence 
+    seq 1 10 | 2041 slippy '4p'        # sequence print additional 4th line
+    seq 1 20 | 2041 slippy '/2./p'     # sequence print additional for 20s      
 
     cd .. && rm -rf temp
 
