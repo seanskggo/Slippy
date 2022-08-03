7a0b3888a7bb49bd2f0260bb97c77c0cb2cd811b
z5310199 <z5310199@cse.unsw.edu.au>
2022-08-03 20:47:58 +1000

    tests done

--- tests/test05.sh
+++ tests/test05.sh
@@ -8,6 +8,8 @@
 # => multiple commands
 #
 # ASSUMES ALL RELEVANT SLIPPY FILES ARE IN PATH
+#
+# TEST MAY NOT ALL PASS AS THIS FEATURE WAS NOT COMPLETED
 ######################################################################
 
 ######################################################################
@@ -18,7 +20,23 @@ test_commands () {
 
     mkdir temp && cp slippy args_parser.py sed_parser.py temp && cd temp || exit
 
-    echo "test" | ./slippy 'q'
+    seq 1 50                        | ./slippy '3p;9q'                  # normal test
+    echo "3p" > commands.slippy
+    echo "6p" >> commands.slippy
+    seq 1 50                        | ./slippy -f commands.slippy       # test with input file 
+    seq 1 50                        | ./slippy '%%%;3p'                 # invalid test (front)
+    seq 1 50                        | ./slippy '3p;%%%'                 # invalid test (back)
+    seq 1 50                        | ./slippy '/3/,/4/p;s/3//'         # range test (front)
+    seq 1 50                        | ./slippy 's/3//;/3/,/4/p;'        # range test (back)
+    seq 1 50                        | ./slippy '3,10s/3//;/3/,/4/p;'    # range test (all)
+    seq 1 50                        | ./slippy '3p;5p;3d;s//4/'         # chaining
+    seq 1 50                        | ./slippy '3p;   5p  ;  3 d   '    # test with spacing
+    seq 1 50                        | ./slippy '3p;5p;3d;'              # test with trailing ;
+    seq 1 50                        | ./slippy '3p;   5p  ;  3 d   ;'   # test with spacing and trailing ; 
+    seq 1 50                        | ./slippy ';3p;   5p  ;  3 d'      # test with spacing and leading ;
+    seq 1 50                        | ./slippy ';3p;5p;3d;'             # test with trailing and leading ;
+    seq 1 50                        | ./slippy '; 3  p ; 5 p ; 3d;'     # test with trailing and leading ; with spacing
+    seq 1 50                        | ./slippy '3p;5p;3d # comment'     # test with trailing and leading ;
 
     cd .. && rm -rf temp
 
@@ -32,8 +50,23 @@ make_answers () {
 
     mkdir temp && cd temp || exit
 
-    echo "test" | 2041 slippy 'q'
-
+    seq 1 50                        | 2041 slippy '3p;9q'                  # normal test
+    echo "3p" > commands.slippy
+    echo "6p" >> commands.slippy
+    seq 1 50                        | 2041 slippy -f commands.slippy       # test with input file 
+    seq 1 50                        | 2041 slippy '%%%;3p'                 # invalid test (front)
+    seq 1 50                        | 2041 slippy '3p;%%%'                 # invalid test (back)
+    seq 1 50                        | 2041 slippy '/3/,/4/p;s/3//'         # range test (front)
+    seq 1 50                        | 2041 slippy 's/3//;/3/,/4/p;'        # range test (back)
+    seq 1 50                        | 2041 slippy '3,10s/3//;/3/,/4/p;'    # range test (all)
+    seq 1 50                        | 2041 slippy '3p;5p;3d;s//4/'         # chaining
+    seq 1 50                        | 2041 slippy '3p;   5p  ;  3 d   '    # test with spacing
+    seq 1 50                        | 2041 slippy '3p;5p;3d;'              # test with trailing ;
+    seq 1 50                        | 2041 slippy '3p;   5p  ;  3 d   ;'   # test with spacing and trailing ; 
+    seq 1 50                        | 2041 slippy ';3p;   5p  ;  3 d'      # test with spacing and leading ;
+    seq 1 50                        | 2041 slippy ';3p;5p;3d;'             # test with trailing and leading ;
+    seq 1 50                        | 2041 slippy '; 3  p ; 5 p ; 3d;'     # test with trailing and leading ; with spacing
+    seq 1 50                        | 2041 slippy '3p;5p;3d # comment'     # test with trailing and leading ;
     cd .. && rm -rf temp
 
 }
